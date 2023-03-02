daf281b82b654adab03691c6c95b95451111a45b
rkapril <rkapril@me.com>
2023-03-02 23:35:17 +0800

    added code into q1 assignment

--- assignment/q1-smart-home.js
+++ assignment/q1-smart-home.js
@@ -7,23 +7,41 @@
 */
 
 // Task: Add code here
+class BaseSignal {
+  constructor() {
+    if (this.constructor.name === "BaseSignal") {
+      throw new Error("This class cannot be instantiated");
+    }
+    this.type;
+  }
+
+  send() {
+    console.log(`Sending ${this.type} signal`);
+  }
+}
 
 class TvSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Add code here
+    super();
+    this.type = "tv";
+  }
 }
 
 class AirconSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Add code here
+    super();
+    this.type = "aircon";
+  }
 }
 
 class DoorSignal extends BaseSignal {
-    constructor(){
-        // Add code here
-    }
+  constructor() {
+    // Add code here
+    super();
+    this.type = "door";
+  }
 }
 
 const tv = new TvSignal();
